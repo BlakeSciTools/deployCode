c2d4db64f5511462488ad0a096c900df484246dd
Blake Knudsen <blake@scitools.com>
2017-04-24 19:46:37 -0600

    reverting

--- sync_manager.py
+++ sync_manager.py
@@ -357,8 +357,6 @@ class SyncManager(BaseHTTPRequestHandler):
 
     def handle201(self):
         self.send_response(201)
-        self.send_header("Access-Control-Allow-Origin", self.headers["Origin"])
-        self.send_header("Access-Control-Allow-Credentials", "true")
         self.end_headers()
 
 
@@ -399,8 +397,7 @@ class SyncManager(BaseHTTPRequestHandler):
 
     def handlePut200(self):
         self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin", self.headers["Origin"])
-        self.send_header("Access-Control-Allow-Credentials", "true")
+        self.send_header("Content-type", "application/json")
         self.end_headers()
 
 
@@ -443,16 +440,12 @@ class SyncManager(BaseHTTPRequestHandler):
     def handle404(self):
         self.send_response(404)
         self.send_header("Content-type", "text/html")
-        self.send_header("Access-Control-Allow-Origin", self.headers["Origin"])
-        self.send_header("Access-Control-Allow-Credentials", "true")
         self.end_headers()
         self.wfile.write(bytes("<strong>Not Found</strong>", "utf-8"))
 
     def handle401(self):
         self.send_response(401)
         self.send_header("Content-type", "text/html")
-        self.send_header("Access-Control-Allow-Origin", self.headers["Origin"])
-        self.send_header("Access-Control-Allow-Credentials", "true")
         self.end_headers()
         self.wfile.write(bytes("<strong>Wrong username or password</strong>", "utf-8"))
 
